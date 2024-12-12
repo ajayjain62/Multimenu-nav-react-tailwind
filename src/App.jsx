4c10351170038240b92052153a9fc3b7100bfd60
ajayjain62 <ajay8290440876@gmail.com>
2024-12-12 15:36:06 +0530

    jk

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Menus } from "./utils";
-import Logo from "./assets/logo.webp";
+import Logo from "../assets/logo.webp";
 import DesktopMenu from "./components/DesktopMenu";
 import MobMenu from "./components/MobMenu";
 
 export default function App() {
   return (
-    
     <div>
-      <header className="h-16 text-[15px] fixed inset-0 flex-center bg-[#18181A] ">
+      <header className="h-16 text-[15px] fixed inset-0 flex-center bg-[white] ">
         <nav className=" px-3.5 flex-center-between w-full max-w-7xl mx-auto">
           <div className="flex-center gap-x-3 z-[999] relative">
-            <img src={Logo} alt="" className="size-8" />
-            <h3 className="text-lg font-semibold">Framer</h3>
+            <img
+              src={Logo}
+              alt=""
+              className="size-8"
+              style={{ width: "12rem", height: "3rem" }}
+            />
+            <h3 className="text-lg font-semibold"></h3>
           </div>
 
           <ul className="gap-x-1 lg:flex-center hidden">
